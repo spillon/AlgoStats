f353534b0166e1a1fdc4bb958f4a9fad0c4df1ff
Pillon <pillonsteve@gmail.com>
2018-04-19 11:26:20 +0200

    maj index

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 			<h1 class="header">AlgoStats</h1>
 		</div>
 		<div class="col-md-6 formulaire">
-			<form method="post">
+			<form action="./insert.php" method="POST">
 				<label class="mr-sm-2" for="inlineFormCustomSelect">AlgoStats</label>
 				<select class="custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
 					<option value="3">Tri par insertion</option>
@@ -25,10 +25,9 @@
 			    	<option value="3">Tri Peigne</option>
 				</select>
 				<br><br>
-				<input type="checkbox" class="custom-control-input">
 			</label>
 			<div class="form-group">
-				<input type="text" name="user" class="form-control" id="login" aria-describedby="loginHelp" placeholder="1; 2; 3; 4">
+				<input type="text" name="nombres" class="form-control" value="1;2;3;4;5;6">
 			</div>
 			<button type="submit" class="btn btn-primary">Go !</button>
 		</form>
