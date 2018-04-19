7897296250c41175b399af9ee50653e8ff10b7d3
Pillon <pillonsteve@gmail.com>
2018-04-19 11:00:25 +0200

    test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First page</title>
+</head>
+<body>
+	<h1>Algo de tri</h1>
+		<form action="/insert.php" method="POST">
+			<select multiple>
+  			<option value="Insertion">Insertion</option>
+  			<option value="Select">Select</option>
+  			<option value="Bulles">Bulles</option>
+  			<option value="Shell">Shell</option>
+  			<option value="Fusion">Fusion</option>
+  			<option value="Rapide">Rapide</option>
+  			<option value="Peigne">Peigne</option>
+			</select><br>
+			<label>merci de mettre les nombres a trier separé de ;</label>
+			<input type="text" name="nombres" value="1;2;3;4;5;6">
+			<input type="submit">
+		</form>
+
+</body>
+</html>
