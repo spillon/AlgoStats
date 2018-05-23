8cfee5804413de00f63ca90561c972cfe50a36ce
Pillon <pillonsteve@gmail.com>
2018-05-23 14:37:33 +0200

    correction peigne

--- home.php
+++ home.php
@@ -29,4 +29,8 @@ else if ($choix == 4)
 {
 	shell($list);
 }
+
+{
+	peigne($list);
+}
 ?>
--- peigne.php
+++ peigne.php
@@ -1,20 +1,35 @@
 <?php
-function insert($list)
+function peigne($list)
 {
-	$popo = count($my_array);
+	echo "Tri peigne: ";
+	$iter = 0;
+	$timestamp_debut = microtime(true);
+	$popo = count($list);
         $swap = true;
 	while ($popo > 1 || $swap){
 		if($popo > 1) $popo /= 1.25;
  		$swap = false;
 		$i = 0;
-		while($i+$popo < count($my_array)){
-			if($my_array[$i] > $my_array[$i+$popo]){
-				list($my_array[$i], $my_array[$i+$popo]) = array($my_array[$i+$popo],$my_array[$i]);
+		while($i+$popo < count($list)){
+			if($list[$i] > $list[$i+$popo]){
+				list($list[$i], $list[$i+$popo]) = array($list[$i+$popo],$list[$i]);
 				$swap = true;
+				$iter++;
 			}
+			$iter++;
 			$i++;
 		}
 	}
-	return $my_array;
+	$timestamp_fin = microtime(true);
+	$difference_ms = $timestamp_fin - $timestamp_debut;
+	$i = 0;
+	echo "<br>"."Voici votre liste triee: ";
+	while ($i < count($list))
+	{
+		print  $list[$i]." ";
+		$i++;
+	}
+	echo "<br>"."Nombre d'iteration: ".$iter;
+	echo "<br>"."Temps d'execution : ". $difference_ms . " secondes.";
 }
 ?>
